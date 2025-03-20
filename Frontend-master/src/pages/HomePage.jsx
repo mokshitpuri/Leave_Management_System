b201338703728e998df1cfc2f716f8dec5d10eeb
mokshitpuri <mokshitp44@gmail.com>
2025-03-20 16:44:07 +0530

    new users

--- Frontend-master/src/pages/HomePage.jsx
+++ Frontend-master/src/pages/HomePage.jsx
@@ -10,8 +10,8 @@ const COLORS = ["#1565C0", "#90CAF9"];
 
 const facultyImages = {
   mokshit: "/images/mokshit.jpg",  
-  harshil: "/images/harshil.jpg",  
-  parth: "/images/parth.jpg",     
+  harshill: "/images/harshil.jpg",  
+  parthh: "/images/parth.jpg",     
 };
 
 // Default profile image if username is not found
